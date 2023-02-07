78e1734d1af4f3c2f4ca44a71bf470984fa985bc
Halepaa <kovaliknazario@gmail.com>
2023-02-07 21:00:25 +0200

    added form validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,8 +2,9 @@ import { AuthService } from '../../services/auth.service';
 import { RegisterUserDto } from '../../../dtos/RegisterUserDto';
 import { GyazoService } from '../../services/gyazo.service';
 import { HttpService } from 'src/app/services/http.service';
-import { EmailValidator, FormBuilder } from '@angular/forms';
+import { AbstractControl, EmailValidator, FormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ErrorStateMatcher } from '@angular/material';
 
 @Component({
   selector: 'app-register',
@@ -16,11 +17,51 @@ export class RegisterComponent implements OnInit {
      private gyazoService: GyazoService, private authService: AuthService) { }
 
   registerForm = this.fb.group({
-    firstName:[''],
-    email:[''],
-    password:[''],
-    confirmPassword:[''],
+    firstName:['',Validators.required],
+    email:['',Validators.email],
+    password:['',[Validators.required,this.atLeastOneUpperCaseLetter(),
+    this.asLeastOneDigit()]], 
+    confirmPassword:['',this.equalsToPassword()],
   })
+
+  private asLeastOneDigit():ValidatorFn{
+    return (control:AbstractControl<string>):ValidationErrors | null => {
+      const chars = [...control.value]
+      if(chars.some(x => !isNaN(Number(x)))){
+        return null;
+      }
+
+      return {'oneDigit':'Password has to have al least one digit'}
+    }
+  }
+  private atLeastOneUpperCaseLetter():ValidatorFn{
+    return (control:AbstractControl<string>):ValidationErrors| null => {
+      const chars = [...control.value]
+      console.log(chars,chars.some(x => x === x.toUpperCase()));
+      if(chars.some(x => x === x.toUpperCase())){
+        return null;
+      }
+
+      return {'upperCaseLetter':'The password has to have at least one uppercase letter'};
+    };
+  }
+
+  private equalsToPassword():ValidatorFn{
+    return (control:AbstractControl<string>):ValidationErrors| null => {
+      const password = control.value;
+      const confirmation = this.registerForm?.get('password')?.value;
+      console.log('sfsfsdf');
+      if(!confirmation){
+        return {'missMatch':'The password and confirmation password does not match'};
+      }
+
+      if(password === confirmation){
+        return null;
+      }
+
+      return {'missMatch':'The password and confirmation password does not match'};
+    };
+  }
 
   
   ngOnInit(): void {
